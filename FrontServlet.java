e9a1dd4de4b31b89bb5ba0f8c1037b6be503e432
WeedWorm075 <weedWorm075@gmail.com>
2023-06-06 23:22:49 +0300

    Sprint2

--- FrontServlet.java
+++ FrontServlet.java
@@ -1,10 +1,20 @@
 package etu1769.framework.servlet;
 
 import java.io.*;
+import java.util.HashMap;
+
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 
 public class FrontServlet extends HttpServlet {
+    HashMap MappingUrls;
+    public HashMap getMappingUrls() {
+        return MappingUrls;
+    }
+    public void setMappingUrls(HashMap mappingUrls) {
+        MappingUrls = mappingUrls;
+    }
+
     public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
         response.getWriter().println("ok");
     }
